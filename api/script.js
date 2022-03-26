f674f1bb524cd94bdb3727225a5d92c947690ff2
ting-prado <ethel.prado1998@gmail.com>
2022-03-26 15:36:56 +0800

    Extended api practice

--- api/script.js
+++ api/script.js
@@ -1,8 +1,20 @@
 const img = document.querySelector('img');
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=valorant', {mode: 'cors'})
-  .then(response => {
-    return response.json();
-  })
-  .then(response => {
-    img.src = response.data.images.original.url;
-  });
+const searchBar = document.querySelector('input');
+
+const loadNewImage = () => {
+  if(searchBar.input != '' || searchBar.input !== null){
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=${searchBar.value}`, {mode: 'cors'})
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      img.src = response.data.images.original.url;
+    })
+    .catch(error => {
+      console.log('An error occured: ' + error);
+    });
+  }
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', loadNewImage);
