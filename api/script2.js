00b88e971583db8b18dfc4882cadf71baab03172
ting-prado <ethel.prado1998@gmail.com>
2022-03-27 18:03:51 +0800

    Converted to async/await functions

---
+++ api/script2.js
@@ -0,0 +1,20 @@
+//Using async/await
+
+const img = document.querySelector('img');
+const searchBar = document.querySelector('input');
+const btn = document.querySelector('button');
+
+const loadNewImage = async () => {
+  try {
+    if(searchBar.input != '' || searchBar.input !== null) {
+      let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=${searchBar.value}`, {mode: 'cors'})
+      let gifData = await response.json();
+      img.src = gifData.data.images.original.url;
+    }
+  }
+  catch (error) {
+    console.log('An error occured: ' + error);
+  }
+}
+
+btn.addEventListener('click', loadNewImage);
