c886f70ee54790b77a7d6c47603afb8987b13f3c
ting-prado <ethel.prado1998@gmail.com>
2022-04-13 13:39:25 +0800

    Applied prettify

--- api/script.js
+++ api/script.js
@@ -5,18 +5,21 @@ const searchBar = document.querySelector('input');
 const btn = document.querySelector('button');
 
 const loadNewImage = () => {
-  if(searchBar.value != '' || searchBar.value !== null){
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=${searchBar.value}`, {mode: 'cors'})
-    .then(response => {
-      return response.json();
-    })
-    .then(response => {
-      img.src = response.data.images.original.url;
-    })
-    .catch(error => {
-      console.log('An error occured: ' + error);
-    });
+  if (searchBar.value != '' || searchBar.value !== null) {
+    fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=${searchBar.value}`,
+      { mode: 'cors' }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        img.src = response.data.images.original.url;
+      })
+      .catch((error) => {
+        console.log('An error occured: ' + error);
+      });
   }
-}
+};
 
 btn.addEventListener('click', loadNewImage);
--- api/script2.js
+++ api/script2.js
@@ -6,15 +6,17 @@ const btn = document.querySelector('button');
 
 const loadNewImage = async () => {
   try {
-    if(searchBar.value != '' || searchBar.value !== null) {
-      let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=${searchBar.value}`, {mode: 'cors'})
+    if (searchBar.value != '' || searchBar.value !== null) {
+      let response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=fMDEL3pkvdFXbLfoRJAo4I9fuVNXmH1T&s=${searchBar.value}`,
+        { mode: 'cors' }
+      );
       let gifData = await response.json();
       img.src = gifData.data.images.original.url;
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log('An error occured: ' + error);
   }
-}
+};
 
 btn.addEventListener('click', loadNewImage);
